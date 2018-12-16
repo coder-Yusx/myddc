9bdeafb380be0e2f7c54e6de7c5df3af99a1248a
yusx <18307202396@163.com>
2018-12-16 20:57:32 +0800

    微信

--- src/main/java/com/zxyy/yusx/myddc/utils/WxApiUtil.java
+++ src/main/java/com/zxyy/yusx/myddc/utils/WxApiUtil.java
@@ -1,6 +1,10 @@
 package com.zxyy.yusx.myddc.utils;
 
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
 
 public class WxApiUtil {
 
@@ -10,8 +14,46 @@ public class WxApiUtil {
     public static String getAccessToken(){
         String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+APPID+"&secret="+APPSECRET;
         String s = "";
+        try {
+            s=Request.Get(url)
+                    .connectTimeout(1000)
+                    .socketTimeout(1000)
+                    .execute().returnContent().asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(s);
-        return s;
+        String access_token = "";
+        if(!s.isEmpty()){
+            JSONObject jo = new JSONObject().getJSONObject(s);
+            access_token = (String)jo.get("access_token");
+
+        }
+        return access_token;
+    }
+
+    public static String getUserInfo(){
+
+        String accessToken = getAccessToken();
+
+        String url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token="+accessToken+"&openid=OPENID&lang=zh_CN";
+        String s = "";
+        try {
+            s=Request.Get(url)
+                    .connectTimeout(1000)
+                    .socketTimeout(1000)
+                    .execute().returnContent().asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(s);
+        String access_token = "";
+        if(!s.isEmpty()){
+            JSONObject jo = new JSONObject().getJSONObject(s);
+            access_token = (String)jo.get("access_token");
+
+        }
+        return access_token;
     }
 
     public static void main(String[] args) {
